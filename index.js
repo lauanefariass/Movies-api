c552e98fe940367bd73428f3a8d1c74fa43e8532
Lauane Farias <Lauanefarias.dev@gmail.com>
2024-08-31 18:31:24 +0400

    feat:create endpoint post

--- index.js
+++ index.js
@@ -15,9 +15,22 @@ const Film = mongoose.model("Film", {
   image_url: String,
   trailer_url: String,
 });
+app.use(express.json());
+
+//post
+app.post("/", async (req, res) => {
+  const film = new Film({
+    title: req.body.title,
+    description: req.body.description,
+    image_url: req.body.image_url,
+    trailer_url: req.body.trailer_url,
+  });
+  await film.save();
+  return res.send("successfully created");
+});
 
 //app
-app.use(express.json());
+
 app.listen(port, () => {
   mongoose.connect("mongodb://localhost:27017/test");
   console.log(`Listening to port ${port}`);
