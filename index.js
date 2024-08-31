a984ab0b7041bbd05b84420dd9cff177a8d00ced
Lauane Farias <Lauanefarias.dev@gmail.com>
2024-08-31 18:06:25 +0400

    feat:create Film Model

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const mongoose = require("mongoose");
+const Film = mongoose.model("Film", {
+  title: String,
+  description: String,
+  image_url: String,
+  trailer_url: String,
+});
 
 //app
 app.use(express.json());
